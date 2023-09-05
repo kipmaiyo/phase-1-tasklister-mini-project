c32151421fc36cea15caffa35e0cbfd8d5709da8
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-09-05 17:51:31 +0300

    recap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form =document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const Tasks = e.target.querySelector("#new-task-description").value
+      buildList(Tasks);
+      form.reset();
+  })
 });
+
+
+
+function buildList(Tasks){
+  
+  let tasksList = document.getElementById("tasks");
+  let li = document.createElement("li");
+  li.textContent = `${Tasks} `;
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "X";
+  li.appendChild(btn);
+  tasksList.appendChild(li);
+}
+
+function handleDelete (e){
+  e.target.parentNode.remove()
+}
